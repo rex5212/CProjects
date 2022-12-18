9b57729d9b585b70083e68bf36e1313a95579a82
rex5212 <xrexv158925lpo@gmail.com>
2022-12-18 13:10:26 -0300

    Fist Code C Program

---
+++ CBegin/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void clearInput() {
+     char c;
+     while ((c = getchar()) != '\n' && c != EOF) {}
+}
+void readText(char *buffer, int length) {
+ fgets(buffer, length, stdin);
+ strtok(buffer, "\n");
+}
+
+int main()
+{
+    /*
+       int - %d || %i (32 bits),
+       long int - %li (32 bits),
+       long long int - %lli (64 bits),
+       float - %f (precisão simples),
+       double - %lf  (precisão dupla),
+       char - %c (caractere),
+       char[] - %s (string),
+       scanf = input, coloca o placeholder respectivo a o tipo de variavel digitada,
+       fgets,
+    */
+
+    int Int;
+    double Double;
+    char Char;
+    char String[50];
+
+    printf("Escreva um nunero: ");
+    scanf("%i", &Int);
+
+    printf("Escreva um numero flutuante: ");
+    scanf("%lf", &Double);
+
+    printf("Escreva um texto: ");
+    clearInput();
+    readText(String, 50);
+
+    // Ta pedindo um  caractere diversas vezes (arrumar)
+    printf("Escreva um caracte: ");
+    clearInput();
+    scanf("%c", &Char);
+
+
+    printf("%i\n", Int);
+    printf("%.2lf\n", Double);
+    printf("%c\n", Char);
+    printf("%s\n", String);
+
+    int w, z;
+    double resultado;
+    w = 5;
+    z = 2;
+    resultado = (double) w / z;
+    printf("O Resultado da divisao e: %.2lf\n", resultado);
+
+ return 0;
+}
